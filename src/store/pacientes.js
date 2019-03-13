0ad7406331aefe9b0a0f9302e45492e5302be3e1
Alex <alejandro.sanchez.queb@gmail.com>
2019-03-12 22:11:38 -0600

    Terminar y agregar el modulo de tareas

--- src/store/pacientes.js
+++ src/store/pacientes.js
@@ -1,10 +1,12 @@
+import { reject } from "q";
+
 const store = {
     states:{
-        usuarios:[]
+        pacientes:[]
     },
     getters:{
        getPacientes(state){
-            return states.usuarios
+            return states.pacientes
        },
        getListaPorID(state){
            return usuarioID =>state.pacientes.filter(paciente => paciente.usuarioID == usuarioID)
@@ -12,11 +14,34 @@ const store = {
     }, 
     mutations:{
         agregarPaciente(state, paciente){
-            
+            states.pacientes.push(paciente)
+        },
+        actualizarPaciente(state, paciente){
+            let index = states.pacientes.findIndex(pac => pac.id == paciente.id )
+            state.pacientes.splice(indice, 1, paciente)
         }
     },
     actions:{
-       
+       cargarPacientes({commit, rootGetters}){
+            return new Promise((resolve, reject) => {
+
+            })
+       },
+       agregarPaciente({},paciente){
+            return new Promise((resolve, reject) => {
+
+            })
+       },
+       eliminarPaciente({}, paciente){
+            return new Promise((resolve, reject) => {
+
+            })
+       },
+       actualizarPaciente({}, paciente){
+        return new Promise((resolve, reject) => {
+
+        })
+   }
     }
 }
 
