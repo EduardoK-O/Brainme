69e935f5ce69b32a8b97594f58c4e2bb3af81ff4
Alex <alejandro.sanchez.queb@gmail.com>
2019-03-13 23:24:00 -0600

    Diseño de prueba finger tapping

---
+++ src/vistas/FingerTapingTest.vue
@@ -0,0 +1,32 @@
+<template>    
+    <v-card
+      class="mx-auto"
+      color="#26c6da"
+      dark
+      max-width="400"
+    >
+      <v-card-title>
+        <v-icon
+          large
+          left
+        >
+          touch_app
+        </v-icon>
+        <span class="title font-weight-light">Finger Tap Test</span>
+      </v-card-title>
+  
+      <v-card-text class="headline font-weight-bold">
+        Por favor, haga tapping en esta zona azul al ritmo que se le pida
+      </v-card-text>      
+    </v-card>  
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
