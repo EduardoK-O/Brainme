485ed2c168f461b9b57c31b9444967d5cc319bfb
Alex <alejandro.sanchez.queb@gmail.com>
2019-03-19 22:59:47 -0600

    fix line 11 in eventos.js

--- src/store/eventos.js
+++ src/store/eventos.js
@@ -8,7 +8,7 @@ const store = {
         }
     }, 
     mutations:{
-        login(states, mostrarDialogoTapping){
+        change(states, mostrarDialogoTapping){
             states.mostrarDialogoTapping = mostrarDialogoTapping
         }
     }
